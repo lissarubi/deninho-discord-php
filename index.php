63a9c84ec402597ea81468b967afdff337b99739
kastrowalker <victorcsa2002@gmail.com>
2020-12-18 13:27:40 -0300

    Criando comando basicos

--- index.php
+++ index.php
@@ -14,13 +14,31 @@ $discord->on('ready', function ($discord) {
 
     // Listen for messages.
     $discord->on('message', function ($message, $discord) {
-        $command = explode(' ', $message->content);
+        $command = strtolower($message->content);
+
+        switch ($command) {
+            case 'bom dia':
+                $message->reply('Bom Dia!');
+                break;
+            case 'boa tarde':
+                $message->reply('Boa Tarde!');
+                break;
+            case 'boa noite':
+                $message->reply('Boa Noite!');
+                break;
+            case 'boas festas':
+                $message->reply('Boas Festas!');
+                break;
+            default:
+                break;
+        }
+
+        $command = explode(' ', $command);
 
         switch ($command[0]) {
             case '*hello':
                 $message->reply('Hello World!');
                 break;
-
             default:
                 break;
         }
